a12f75f7770b5813b5e203e1c49edc7394d087b9
yelim <97503433+97yelim@users.noreply.github.com>
2022-11-07 01:41:46 +0900

    상태관리

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,11 @@ import './App.css';
 import Greetings from './Greetings';
 import Counter from './Counter';
 import MyForm from './MyForm';
+import ReducerSample from './ReducerSample';
 
 const App: React.FC = () => {
-  const onSubmit = (form: {name: string, description: string}) => {
-    console.log(form)
-  }
-  return <MyForm onSubmit={onSubmit}/>
+  
+  return <ReducerSample/>
    
 }
 
--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,9 +1,23 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
+
+type Action = { type: 'INCREASE'} | {type: 'DECREASE'}
+
+const reducer = (state: number, action: Action): number => {
+    switch(action.type) {
+        case 'INCREASE':
+            return state + 1;
+        case 'DECREASE':
+            return state - 1;
+        default:
+            throw new Error('Unhandled action');
+    }
+}
 
 const Counter = () => {
-    const [count, setCount] = useState<number>(0); // useState(0); 만 사용해도 괜찮음. 알아서 유추하기 때문. Generics 는 null 일 수도 있고 아닐 수도 있을 때 사용.
-    const onIncrease = () => setCount(count + 1);
-    const onDecrease = () => setCount(count - 1);
+    //const [count, setCount] = useState<number>(0); // useState(0); 만 사용해도 괜찮음. 알아서 유추하기 때문. Generics 는 null 일 수도 있고 아닐 수도 있을 때 사용.
+    const [count, dispatch] = useReducer(reducer, 0);
+    const onIncrease = () => dispatch({type: 'INCREASE'})
+    const onDecrease = () => dispatch({type: 'DECREASE'})
     return (
         <div>
             <h1>
